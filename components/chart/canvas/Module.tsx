2b8bd5f01d53a964d902ff3c135d30568980aa88
Ranim <106822042+RanimHelamiah@users.noreply.github.com>
2023-12-27 22:51:15 +0300

    Correct modifications

--- components/chart/canvas/Module.tsx
+++ components/chart/canvas/Module.tsx
@@ -33,15 +33,60 @@ const ModuleIconImg = styled.div`
       box-shadow: 0px 0px 7px 0px #00599f;
     }
   }
-
   .subTitle {
     font-size: 12px;
     margin-top: -3px;
     font-weight: 700;
   }
 `;
+const ModuleStyle = styled.div`
+  border-radius: 40%;
+  padding:7px;
+  position: absolute;
+  overflow: hidden;
+  cursor: move;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-size: contain;
+  .moduleIcon {
+    text-align: center;
+    position: absolute;
+    top: 3px;
+    left: 3px;
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+  .moduleNameCon{
+    padding: 3px;
+    color: #fff;
+    position: relative;
+    font-size: 15px;
+    font-weight: 700;
+    background-color: #699041;
+    height: 85px;
+    width: 85px;
+    display: flex;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50px;
+    &.active {
+      border: 2px solid #00599f;
+      -webkit-box-shadow: 0px 0px 7px 0px #00599f;
+      -moz-box-shadow: 0px 0px 7px 0px #00599f;
+      box-shadow: 0px 0px 7px 0px #00599f;
+    }
+  }
+  .subTitle {
+    font-size: 12px;
+    font-weight: 700;
+    margin-top:-3px;
+  }
+`;
 function Module({ module, selectModule, isSelected }: ModuleProps) {
-  const moduleName = module.title +' '+ module.sub_title  || ModuleInfo.getModuleName(module.type);
+  const moduleName = module.title || ModuleInfo.getModuleName(module.type);
   const onDragStart = useCallback((id, ev) => {
     ev.dataTransfer.setData('dragType', 'moveModule');
     ev.dataTransfer.setData('id', id);
@@ -52,25 +97,24 @@ function Module({ module, selectModule, isSelected }: ModuleProps) {
   return (
     <>
       {module.icon ? (
-          <ModuleIconImg
-            style={{ top: module.y, left: module.x, zIndex: module.id }}
-            onDragStart={(ev) => onDragStart(module.id, ev)}
-            onClick={(e) => {
-              if (e.shiftKey) selectModule(module, true);
-              else selectModule(module, false);
-            }}
-          >
-            {module.icon && (
-              <>
-                <img src={module.icon} alt="Module Icon" className={`IconImg ${isSelected ? 'active' : ''} `} />
-                <p className='subTitle'>{module.sub_title}</p>
-              </>
-            )}
-          </ModuleIconImg>
+        <ModuleIconImg
+          style={{ top: module.y, left: module.x, zIndex: module.id }}
+          onDragStart={(ev) => onDragStart(module.id, ev)}
+          onClick={(e) => {
+            if (e.shiftKey) selectModule(module, true);
+            else selectModule(module, false);
+          }}
+        >
+          {module.icon && (
+            <>
+              <img src={module.icon} alt="Module Icon" className={`IconImg ${isSelected ? 'active' : ''} `} />
+              <p className='subTitle'>{module.sub_title}</p>
+            </>
+          )}
+        </ModuleIconImg>
       ) : (
-        <div
-          className={`module ${isSelected ? 'active' : ''
-            } ${module.type.toLowerCase()}`}
+        <ModuleStyle
+          className={`${module.type.toLowerCase()}`}
           key={module.id}
           draggable="true"
           style={{ top: module.y, left: module.x, zIndex: module.id }}
@@ -83,13 +127,13 @@ function Module({ module, selectModule, isSelected }: ModuleProps) {
           <div className="moduleIcon">
             <i className={ModuleInfo.getIcon(module.type) + ' moduleIcon '}></i>
           </div>
-          <div className="moduleNameCon">
+          <div className={`moduleNameCon ${isSelected ? 'active' : ''} `}>
             {moduleName.length >= 34
               ? moduleName.substring(0, 34) + '...'
               : moduleName}
           </div>
-        </div>
-
+          <p className='subTitle'>{module.sub_title}</p>
+        </ModuleStyle>
       )}
     </>
   );
