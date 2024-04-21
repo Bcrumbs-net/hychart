c25fc166297915cc44e126f8fabbab775e4c936d
Ahmad Mhaish <ahmad@dconfig.com>
2024-04-21 21:55:13 +0300

    Updates

--- components/chart/header/tagsInput.tsx
+++ components/chart/header/tagsInput.tsx
@@ -1,229 +1,114 @@
-import React, { useState, useEffect } from 'react';
-import { WithContext as ReactTags } from 'react-tag-input';
+import React, { useCallback, useState } from 'react';
+import Multiselect from 'multiselect-react-dropdown';
 import styled from 'styled-components';
 
 const BCTagsInputWrapper = styled.div`
   display: flex;
   align-items: center;
   height: 50px;
-  justify-content: flex-end; 
-  .selectedClass {
-    display: flex;
-    align-items: center;
+  justify-content: flex-end;
+
+  input {
     height: 20px;
-    padding: 9px 4px 9px 40px;
-    border: none !important;
+    width: auto;
+    padding: 0;
+    margin: 0;
+    ::placeholder {
+      color: #000;
+      opacity: 1; /* Firefox */
+    }
   }
-  .tagInputClass {
-    margin-left: 50px;
-  }
-  .tagInputFieldClass {
+  .searchWrapper {
     background-color: #fff;
-    border: solid 1px var(--bc-secondary-light-hover);
+    border: 1px solid #000;
     border-radius: 20px;
-    height: 20px;
-    width: 120px;
-    color: var(--bc-secondary-light-hover);
-  }
-  .tagClass {
-    display: flex;
-    align-items: center;
-    padding-left: 8px;
-    font-size: 15px;
-    font-weight: bold;
-    border: 1px solid #bfa37cf7;
-    box-shadow: 0px 0px 2px 0px rgb(100, 57, 0);
-    border-radius: 20px;
-    margin: 2px;
-    height: 25px;
-    font-size: 10px;
-    background-color: #eee6dbf7;
-  }
-  .removeClass {
-    display: flex;
-    align-items: center;
-    height: 15px;
-    border: none !important;
-    background-color: transparent !important;
-    color: #444 !important;
-    cursor: pointer;
-  }
-  .suggestionsClass {
-    position: fixed;
-    z-index: 1000;
-    ul {
-      border-radius: 10px;
-      margin-top: -10px;
-      height: 500px; 
-      width: 160px; 
-      overflow-y: auto; /* Add vertical scrollbar when content overflows */
-      overflow-x: hidden; /* Hide horizontal scrollbar if any */
-      scrollbar-width: thin;
-      scrollbar-color: #c8c1c1 transparent;
-      li {
-        color: #000;
-        font-size: 13px;
-        background-color: #f3eded;
-        width: 400px;
-        padding: 9px 4px 9px 40px;
-        display: block;
-        cursor: pointer;
-        height: 45px;
-        font-weight: 400;
-      }
-      li:first-of-type {
-        border-radius: 5px 20px 0 0;
-      }
-      li:last-child {
-        border-radius: 0 0 5px 20px;
-      }
-      li:first-child:last-child {
-        border-radius: 8px 15px 8px 18px;
-      }
-      li:hover {
-        background-color: #c8c1c1;
-      }
+    min-height: auto;
+    padding: 4px;
+    chip {
+      margin-bottom: 0;
+      padding: 2px 8px;
     }
   }
 `;
 
+const SystemTags = [
+  'العقيدة',
+  'الفقه',
+  'التفسير',
+  'الحديث',
+  'القراءات',
+  'أصول التفسير',
+  'أصول الفقه',
+  'النحو والصرف',
+  'البلاغة',
+  'المنطق',
+  'مصطلح الحديث',
+  'القواعد الفقهية',
+  'التاريخ',
+  'التراجم',
+  'أصول الدين',
+];
+
+const removeTagsFromURL = () => {
+  const queryParams = new URLSearchParams(window.location.search);
+  queryParams.delete('tags');
+  const currentURL = window.location.href;
+  let updatedURL = currentURL.split('?')[0];
+
+  if (queryParams.toString()) {
+    updatedURL += `?${queryParams.toString()}`;
+  }
+
+  window.history.pushState({ path: updatedURL }, '', updatedURL);
+};
+
+const handleUpdateURL = (tags) => {
+  const tagNames = tags.map((tag) => tag.name);
+  const queryParams = new URLSearchParams(window.location.search);
+  queryParams.set('tags', tagNames.join(', '));
+  const currentURL = window.location.href;
+  const updatedURL = `${currentURL.split('?')[0]}?${queryParams.toString()}`;
+  window.history.pushState({ path: updatedURL }, '', updatedURL);
+};
 
 const TagsInput = ({ predefinedTags }) => {
-    const [tags, setTags] = useState([]);
-    const [currentInput, setCurrentInput] = useState('');
-    predefinedTags = [
-        'العقيدة',
-        'الفقه',
-        'التفسير',
-        'الحديث',
-        'القراءات',
-        'أصول التفسير',
-        'أصول الفقه',
-        'النحو والصرف',
-        'البلاغة',
-        'المنطق',
-        'مصطلح الحديث',
-        'القواعد الفقهية',
-        'التاريخ',
-        'التراجم',
-        'أصول الدين'
-    ];
-    const handleDelete = (index) => {
-        const newTags = [...tags];
-        newTags.splice(index, 1);
-        setTags(newTags);
-        if (newTags.length === 0) {
-            removeTagsFromURL();
-        } else {
-            handleUpdateURL(newTags);
-        }
-    };
+  const [tags, setTags] = useState([]);
 
-    const removeTagsFromURL = () => {
-        const queryParams = new URLSearchParams(window.location.search);
-        queryParams.delete('tags');
-        const currentURL = window.location.href;
-        let updatedURL = currentURL.split('?')[0];
+  const onAdd = useCallback(
+    (selectedList: any, selectedItem: any) => {
+      const newTags = [...tags, selectedItem];
+      setTags(newTags);
+      handleUpdateURL(newTags);
+    },
+    [tags]
+  );
 
-        if (queryParams.toString()) {
-            updatedURL += `?${queryParams.toString()}`;
-        }
-
-        window.history.pushState({ path: updatedURL }, '', updatedURL);
-    };
-    const handleAddition = (tag) => {
-        const lowerCaseTag = typeof tag.text === 'string' ? tag.text.toLowerCase() : '';
-        const tagExists = predefinedTags.some(
-            (predefinedTag) => predefinedTag.toLowerCase() === lowerCaseTag
-        );
-        if (tagExists && tags.length < 3) {
-            const newTags = [...tags, tag];
-            setTags(newTags);
-            handleUpdateURL(newTags);
-        }
-    };
-
-    const handleDrag = (tag, currPos, newPos) => {
-        const newTags = [...tags];
-        newTags.splice(currPos, 1);
-        newTags.splice(newPos, 0, tag);
-        setTags(newTags);
+  const onRemove = useCallback(
+    (selectedList: any, selectedItem: string) => {
+      const newTags = tags.filter((m) => m !== selectedItem);
+      setTags(newTags);
+      if (newTags.length === 0) {
+        removeTagsFromURL();
+      } else {
         handleUpdateURL(newTags);
-    };
+      }
+    },
+    [tags]
+  );
 
-    const handleTagClick = (index) => {
-        console.log('Tag clicked:', tags[index]);
-    };
-
-    const handleUpdateURL = (tags) => {
-        const tagNames = tags.map((tag) => tag.text);
-        const queryParams = new URLSearchParams(window.location.search);
-        queryParams.set('tags', tagNames.join(', '));
-        const currentURL = window.location.href;
-        const updatedURL = `${currentURL.split('?')[0]}?${queryParams.toString()}`;
-        window.history.pushState({ path: updatedURL }, '', updatedURL);
-    };
-
-    const [autoCompleteList, setAutoCompleteList] = useState([]);
-    const [showAutoComplete, setShowAutoComplete] = useState(false);
-
-    const handleAutocompleteSelect = (value) => {
-        setCurrentInput(value);
-        setShowAutoComplete(false);
-    };
-
-    useEffect(() => {
-        const fetchAutoCompleteList = async () => {
-            await new Promise((resolve) => setTimeout(resolve, 1000));
-            setAutoCompleteList(predefinedTags);
-        };
-
-        fetchAutoCompleteList();
-    }, [predefinedTags]);
-
-    return (
-        <>
-            <BCTagsInputWrapper>
-                <ReactTags
-                    suggestions={predefinedTags.map((tag) => ({ id: tag, text: tag }))}
-                    tags={tags}
-                    handleDelete={handleDelete}
-                    handleAddition={handleAddition}
-                    handleDrag={handleDrag}
-                    handleTagClick={handleTagClick}
-                    autofocus={false}
-                    onKeyDown={(event) => {
-                        if (event.key === 'Enter') {
-                            handleAddition({ id: currentInput, text: currentInput });
-                            setCurrentInput('');
-                        }
-                    }}
-                    placeholder='Press to add tag'
-                    classNames={{
-                        tags: 'tagsClass',
-                        tagInput: 'tagInputClass',
-                        tagInputField: 'tagInputFieldClass',
-                        selected: 'selectedClass',
-                        tag: 'tagClass',
-                        remove: 'removeClass',
-                        suggestions: 'suggestionsClass',
-                    }}
-                />
-            </BCTagsInputWrapper>
-            {showAutoComplete && autoCompleteList && autoCompleteList.length ? (
-                <ul>
-                    {autoCompleteList.map((val, index) => (
-                        <li
-                            key={`AutoCompleteItem${index}`}
-                            onClick={() => handleAutocompleteSelect(val)}
-                        >
-                            <span>{val}</span>
-                        </li>
-                    ))}
-                </ul>
-            ) : null}
-        </>
-    );
+  return (
+    <BCTagsInputWrapper>
+      <Multiselect
+        options={SystemTags.map((m) => ({ name: m }))}
+        displayValue="name"
+        isObject={true}
+        selectedValues={tags}
+        onSelect={onAdd}
+        onRemove={onRemove}
+        placeholder="Select tags"
+      />
+    </BCTagsInputWrapper>
+  );
 };
 
 export default TagsInput;
