6a20a80b0effb319812cea1dc2f3b2b82b3e9d4b
Ahmad Mhaish <ahmad@dconfig.com>
2024-08-28 00:26:46 +0300

    debt - fixes BCR-19 Adding Sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://5087daa36e856f07886398193ab98f2d@o4505459717570560.ingest.us.sentry.io/4507851359780864",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
